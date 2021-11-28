d1ee734ac566512a027846649ad48853b8300f59
jorav <joravkumar@gmail.com>
2021-11-29 00:21:21 +0530

    variables & function finished, branches started

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,13 @@ fn main() {
     }
     println!("The value of x is {}", x);
 
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", tup.2);
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("The value of first index of array is: {}", a[1]);
+
 }
